fd482ce35a7c34da000cdee633f72e1f6c9df142
Line Kargaard <lika62310@edu.ucl.dk>
2024-09-17 14:22:31 +0200

    Models created

---
+++ RegionSyd_Team8/Models/AssignmentRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionSyd_Team8.Models
+{
+    public class AssignmentRepository
+    {
+        public ObservableCollection<Assignment> Assignments { get; set; }
+
+        public AssignmentRepository()
+        {
+            Assignments = new ObservableCollection<Assignment>();
+        }
+
+        public void AddAssignment(Assignment assignment)
+        {
+            Assignments.Add(assignment);
+        }
+
+        public void RemoveAssignment(Assignment assignment)
+        {
+        }
+
+        public void UpdateAssignment(Assignment assignment)
+        {
+            //implementation
+        }
+    }
+}
---
+++ RegionSyd_Team8/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionSyd_Team8.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
