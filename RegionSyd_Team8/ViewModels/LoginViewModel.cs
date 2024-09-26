275f54c3ec451964d3781de65a54bf2b71b3c573
yolandeomam <yeto63253@edu.ucl.dk>
2024-09-26 09:55:25 +0200

    Loginwindow som åbner ikke mainwindow og ikke modtager brugernavn

---
+++ RegionSyd_Team8/ViewModels/LoginViewModel.cs
@@ -0,0 +1,92 @@
+﻿using RegionSyd_Team8.Models;
+using RegionSyd_Team8.MVVM;
+using RegionSyd_Team8.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace RegionSyd_Team8.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _username;
+        private string _password;
+        private string _errorMessage;
+        public UserRepository _userRepository { get; set; }
+        private Visibility _errorVisibility;
+
+        public ICommand LoginCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            _userRepository = new UserRepository();
+            ErrorVisibility = Visibility.Collapsed;
+            LoginCommand = new RelayCommand(param => Login());
+        }
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Visibility ErrorVisibility
+        {
+            get { return _errorVisibility; }
+            set
+            {
+                _errorVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void Login()
+        {
+            var user = _userRepository.FindUser(Username, Password);
+            if (user != null)
+            {
+                var mainWindow = new MainWindow();
+                mainWindow.Show();
+
+                MessageBox.Show("MainWindow should be visible now.");//Debug
+                Application.Current.Windows[0]?.Close();
+            }
+            else
+            {
+                ErrorMessage = "Ugyldigt brugernavn eller adgangskode!";
+                ErrorVisibility = Visibility.Visible;
+            }
+        }
+
+
+
+    }
+} 
